0529c56a54214ce487d5ce96da1bb1ce4bdc06b9
Endurance-Peter <endurance.peter@cyphercrescent.com>
2021-09-27 17:48:40 +0100

    update bank branch repository

--- Bank/Bank.NHibernate/Repositories/BankBranchRepository.cs
+++ Bank/Bank.NHibernate/Repositories/BankBranchRepository.cs
@@ -2,14 +2,21 @@
 using Bank.Domain.Repository;
 using NHibernate;
 using ShareProjects.Share.Repositories;
+using ShareProjects.Share.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Bank.NHibernate.Repositories
 {
     public class BankBranchRepository : Repository<BankBranch>, IBankBranchRepository
     {
         public BankBranchRepository(ISession session):base(session) { }
+
+        public Task<ActionResult<BankBranch>> FetchByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Bank/ClassLibrary1/Repository/IBankBranchRepository.cs
+++ Bank/ClassLibrary1/Repository/IBankBranchRepository.cs
@@ -1,9 +1,12 @@
 ﻿using Bank.Domain.Models.Branch;
 using NHibernate.Configuration.Repositories;
+using ShareProjects.Share.Utilities;
+using System.Threading.Tasks;
 
 namespace Bank.Domain.Repository
 {
     public interface IBankBranchRepository : IRepository<BankBranch>
     {
+        Task<ActionResult<BankBranch>> FetchByEmail(string email);
     }
 }
